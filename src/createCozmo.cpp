2f4c64dbc7a661be373b3cef253b0b1d1705a4ee
Vinitha Ranganeni <vrangane@andrew.cmu.edu>
2016-11-14 16:07:37 -0500

    cube cozmo working in rviz

--- src/createCozmo.cpp
+++ src/createCozmo.cpp
@@ -1,19 +1,37 @@
 #include "cozmo/cozmo.hpp"
 #include "dart/dart.hpp"
 #include "dart/gui/gui.hpp"
+#include "ros/ros.h"
+#include <aikido/rviz/InteractiveMarkerViewer.hpp>
 
-using WorldPtr = dart::simulation::WorldPtr;
+// using WorldPtr = dart::simulation::WorldPtr;
+static const std::string topicName("dart_markers");
 
 int main(int argc, char* argv[])
 {
   dart::dynamics::SkeletonPtr coz = cozmo::createCubeCozmo();
-  WorldPtr world(new dart::simulation::World);
-  world->addSkeleton(coz);
 
-  dart::gui::SimWindow win;
-  win.setWorld(world);
+  // Start the RViz viewer.
+  std::cout << "Starting ROS node." << std::endl;
+  ros::init(argc, argv, "load_cozmo");
 
-  glutInit(&argc, argv);
-  win.initWindow(640, 480, "Cube Cozmo");
-  glutMainLoop();
+  std::cout << "Starting viewer. Please subscribe to the '" << topicName
+	    << "' InteractiveMarker topic in RViz." << std::endl;
+  aikido::rviz::InteractiveMarkerViewer viewer(topicName);
+  viewer.addSkeleton(coz);
+  viewer.setAutoUpdate(true);
+
+  std::cout << "Press <Ctrl> + C to exit." << std::endl;
+  ros::spin();
+
+  // DART crappy viewer
+  // WorldPtr world(new dart::simulation::World);
+  // world->addSkeleton(coz);
+
+  // dart::gui::SimWindow win;
+  // win.setWorld(world);
+
+  // glutInit(&argc, argv);
+  // win.initWindow(640, 480, "Cube Cozmo");
+  // glutMainLoop();
 }
