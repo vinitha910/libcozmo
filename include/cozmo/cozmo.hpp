38982bf0d682680c5bbdffbc1e6b6fc580483f1a
Vinitha Ranganeni <vrangane@andrew.cmu.edu>
2016-12-14 16:57:00 -0500

    Fixed issues

--- include/cozmo/cozmo.hpp
+++ include/cozmo/cozmo.hpp
@@ -11,12 +11,25 @@ using InverseKinematicsPtr = dart::dynamics::InverseKinematicsPtr;
 class Cozmo
 {
 public:
+  /// Creates Cozmo object
+
+  /// \param mesh_dir path to the libcozmo/meshes folder
   Cozmo(const std::string& mesh_dir);
-  void setForkliftPosition(double pos);
+
+  /// Returns SkeletonPtr to cozmo
+  /// Though Cozmo only has a 1 DOF forklift, it is modelled as 6 DOF due
+  /// to the inability to model the 4 bar linkage. Therefore, setting
+  /// joint positions on this skeleton will NOT have the expected result.
   SkeletonPtr getCozmoSkeleton() { return cozmo; };
+  
+  /// Takes in pos (angle in radians) and sets forklift position
+  void setForkliftPosition(double pos);
 
 private:
+  /// SkeletonPtr to Cozmo
   SkeletonPtr cozmo;
+
+  // BodyNodePtr to various links on Cozmo skeleton
   BodyNodePtr head;
   BodyNodePtr base;
   BodyNodePtr forklift;
@@ -25,12 +38,40 @@ private:
   BodyNodePtr lower_forklift_strut_right;
   BodyNodePtr upper_forklift_strut_left;
   BodyNodePtr upper_forklift_strut_right;
+
+  /// InverseKinematicsPtr to Inverse Kinematics Module
   InverseKinematicsPtr ik;
+
+  /// Creates and returns a BodyNodePtr to Cozmo's base
   
-  BodyNodePtr makeRootBody(const SkeletonPtr& cozmo, const std::string& name, const std::string& mesh_dir);
-  BodyNodePtr addBody(const SkeletonPtr& cozmo, BodyNodePtr parent, const std::string& name, const std::string& mesh_dir,
-	              Eigen::Vector3d transformFromParent, Eigen::Vector3d transformFromChild);
+  /// \param cozmo SkeletonPtr to cozmo 
+  /// \param mesh_name name of the mesh file without the extension
+  /// \param mesh_dir path to the libcozmo/meshes folder
+  BodyNodePtr makeRootBody(const SkeletonPtr& cozmo,
+			   const std::string& mesh_name,
+			   const std::string& mesh_dir);
+
+  /// Helper method to create an individual link to the base; returns BodyNodePtr to body
+  
+  /// \param cozmo SkeletonPtr to cozmo 
+  /// \param parent parent body node
+  /// \param mesh_name name of the mesh file without the extension
+  /// \param mesh_dir path to the libcozmo/meshes folder
+  /// \param transformFromParent body's transform from parent body node
+  /// \param transformFromChild body's transform from child body node 
+  BodyNodePtr addBody(const SkeletonPtr& cozmo,
+		      BodyNodePtr parent,
+		      const std::string& mesh_name,
+		      const std::string& mesh_dir,
+	              Eigen::Vector3d transformFromParent,
+		      Eigen::Vector3d transformFromChild);
+
+  /// Creates and returns SkeletonPtr to Cozmo skeleton
+
+  /// \param mesh_dir path to the libcozmo/meshes folder
   SkeletonPtr createCozmo(const std::string& mesh_dir);
+
+  /// Creates IK Module on ghost strut
   void createIKModule();
 };
 }
