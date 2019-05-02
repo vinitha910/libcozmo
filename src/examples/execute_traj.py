77aa32b3d4c64f6ad0a5c922fda22d7072065523
Vinitha Ranganeni <vinitha.ranganeni@gmail.com>
2019-05-02 16:38:38 -0700

    Added more cozmopy python bindings; finished python execute trajectory example script

--- src/examples/execute_traj.py
+++ src/examples/execute_traj.py
@@ -2,41 +2,50 @@
 
 import rospy
 from roscpp_initializer import roscpp_initializer
-# from moveit_ros_planning_interface._moveit_roscpp_initializer import roscpp_init
-import aikidopy
+from aikidopy import SkeletonMarker, InteractiveMarkerViewer
 from cozmopy import Cozmo, Waypoint
 from math import pi 
+from datetime import timedelta
+import argparse
 
 if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--mesh_dir', dest='mesh_dir', required = True,
+	                     help='The path to the directory containing Cozmos meshes')
+	args = parser.parse_args()
 
+	# rospy.init_node does not initialize roscpp and if roscpp is not initialized
+	# instanciating ros::NodeHandle will lead to a fatal error.
+	# roscpp_initializer initializes roscpp and ros::NodeHandle in the background
 	roscpp_initializer.roscpp_init("load_cozmo", [])
 	rospy.init_node("load_cozmo")
-	rate = rospy.Rate(1) # 10hz
+	rate = rospy.Rate(10)
+
+	topicName = "dart_markers"
+	baseFrameName = "map"
 
 	if not rospy.is_shutdown():
-		topicName = "dart_markers"
-		baseFrameName = "map"
+		cozmo = Cozmo(args.mesh_dir)
+		skeleton = cozmo.getCozmoSkeleton();
 
-		mesh_dir = " /home/vinitha/workspaces/cozmo_ws/src/libcozmo/src/cozmo_description/meshes"
-		cozmo = Cozmo(mesh_dir)
+		print("Starting viewer. Please subscribe to the {} InteractiveMarker" 
+			  " topic in Rviz".format(topicName))
 
-		print("Starting viewer. Please subscribe to the %s InteractiveMarker topic in Rviz", topicName)
-
-		viewer = aikidopy.InteractiveMarkerViewer(topicName, baseFrameName)
-		viewer.addSkeleton(cozmo.getCozmoSkeleton())
-		viewer.setAutoUpdate(true);
+		viewer = InteractiveMarkerViewer(topicName, baseFrameName)
+		cozmo_marker = viewer.addSkeleton(skeleton)
+		viewer.setAutoUpdate(True);
 
 		waypoints = [
-			Wapoint(0.0, 0.0, 0, 0),
-		    Wapoint(0.3, 0.0, 0, 2),
-		    Wapoint(0.3, 0.0, pi/2, 3),
-		    Wapoint(0.3, 0.3, pi/2, 5),
-		    Wapoint(0.3, 0.3, pi, 6),
-		    Wapoint(0.0, 0.3, pi, 8),
-		    Wapoint(0.0, 0.3, -pi/2, 9),
-		    Wapoint(0.0, 0.0, -pi/2, 11),
-		    Wapoint(0.0, 0.0, 0, 12),
+			Waypoint(0.0, 0.0, 0, 0),
+		    Waypoint(0.3, 0.0, 0, 2),
+		    Waypoint(0.3, 0.0, pi/2, 3),
+		    Waypoint(0.3, 0.3, pi/2, 5),
+		    Waypoint(0.3, 0.3, pi, 6),
+		    Waypoint(0.0, 0.3, pi, 8),
+		    Waypoint(0.0, 0.3, -pi/2, 9),
+		    Waypoint(0.0, 0.0, -pi/2, 11),
+		    Waypoint(0.0, 0.0, 0, 12),
 		]
 
 		traj = cozmo.createInterpolatedTraj(waypoints);
-		cozmo.executeTrajectory(6, traj)	
+		cozmo.executeTrajectory(timedelta(milliseconds=6), traj)	
