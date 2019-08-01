e1e707a13a7a5d0ffed43a86537048f27c463be2
Eric Pan <ericpan0207@gmail.com>
2019-07-31 19:08:50 -0700

    Starting to make the helper functions for oos

--- src/action_space/cozmo_actions.cpp
+++ src/action_space/cozmo_actions.cpp
@@ -3,17 +3,34 @@
 #include <iostream>
 #include <iterator>
 #include <list>
+#include <math.h>
 #include <numeric>
 #include <vector>
 
-struct action {
-    float lin_vel;
-    float ang_vel;
-    float duration;
+struct Action
+{
+    double lin_vel;
+    double ang_vel;
+    double duration;
 };
 
-// For generating a sequence of equidistant values 
-struct double_inc_iterator : std::iterator<std::forward_iterator_tag, double> {
+struct Pose
+{
+    double pos_x;
+    double pos_y;
+    double pos_z;
+    double angle_z;
+};
+
+struct Object_Oriented_Action
+{
+    Pose pose;
+    Action action;
+};
+
+// For generating a sequence of equidistant values
+struct double_inc_iterator : std::iterator<std::forward_iterator_tag, double>
+{
     double_inc_iterator(double initial, double inc = 1.0) : _value(initial), _inc(inc) {}
     value_type operator*() const { return _value; }
     double_inc_iterator& operator++() { _value += _inc; return *this; }
@@ -25,17 +42,37 @@ struct double_inc_iterator : std::iterator<std::forward_iterator_tag, double> {
     value_type _inc;
 };
 
-class GenericActionSpace {
+// Utility function to generate [num] number of choices from [start] to [stop]
+// include_zero : True to add zero to choices, False to not
+//                This allows for 0 verlocity in either the linear or angular direction
+std::vector<double> create_choices(double start, double stop, int num, bool include_zero)
+{
+    double step = (stop - start) / (num - 1);
+    std::vector<double> choices(double_inc_iterator(start, step), double_inc_iterator(stop + FLT_EPSILON));
+    if (include_zero) {
+        bool contains_zero = false;
+        if (std::find(choices.begin(), choices.end(), 0) != choices.end()) {
+            contains_zero = true;
+        }
+        if (!contains_zero) {
+            choices.insert(choices.begin(), 0);
+        }
+    }
+    return choices;
+}
+
+class GenericActionSpace
+{
     public:
-        GenericActionSpace(float lin_min, 
-                           float lin_max, 
-                           float lin_samples, 
-                           float ang_min,
-                           float ang_max,
-                           float ang_samples,
-                           float dur_min,
-                           float dur_max,
-                           float dur_samples)
+        GenericActionSpace(double lin_min,
+                           double lin_max,
+                           double lin_samples,
+                           double ang_min,
+                           double ang_max,
+                           double ang_samples,
+                           double dur_min,
+                           double dur_max,
+                           double dur_samples)
         : lin_min(lin_min),
           lin_max(lin_max),
           lin_samples(lin_samples),
@@ -48,65 +85,113 @@ class GenericActionSpace {
         { generate_actions();
         }
 
-        std::list<action> get_action_space() 
+        std::list<Action> get_action_space()
         {
             return actions;
         }
 
     private:
-        float lin_min{ 0 }; 
-        float lin_max{ 0 };
-        float lin_samples{ 0 };
-        float ang_min{ 0 };
-        float ang_max{ 0 };
-        float ang_samples{ 0 };
-        float dur_min{ 0 };
-        float dur_max{ 0 };
-        float dur_samples{ 0 };
-        std::list<action> actions;
+        double lin_min{ 0 };
+        double lin_max{ 0 };
+        double lin_samples{ 0 };
+        double ang_min{ 0 };
+        double ang_max{ 0 };
+        double ang_samples{ 0 };
+        double dur_min{ 0 };
+        double dur_max{ 0 };
+        double dur_samples{ 0 };
+        std::list<Action> actions;
 
-        void generate_actions() 
+        void generate_actions()
         {
             std::vector<double> lin_choices = create_choices(lin_min, lin_max, lin_samples, true);
             std::vector<double> ang_choices = create_choices(ang_min, ang_max, ang_samples, true);
             std::vector<double> dur_choices = create_choices(dur_min, dur_max, dur_samples, false);
-            
+
             for (double dur_choice : dur_choices) {
                 for (double lin_choice : lin_choices) {
                     for (double ang_choice : ang_choices) {
                         if (!(lin_choice == ang_choice && lin_choice == 0) || dur_choice == 0) {
-                            action a;
-                            a.lin_vel = lin_choice;
-                            a.ang_vel = ang_choice;
-                            a.duration = dur_choice;
+                            Action a {lin_choice, ang_choice, dur_choice};
                             actions.push_back(a);
                         }
                     }
                 }
             }
-    }
-
-    // Helper function to generate [num] number of choices from [start] to [stop]
-    // include_zero : True to add zero to choices, False to not
-    //                This allows for 0 verlocity in either the linear or angular direction
-    std::vector<double> create_choices(double start, double stop, int num, bool include_zero) {
-        double step = (stop - start) / (num - 1);
-        std::vector<double> choices(double_inc_iterator(start, step), double_inc_iterator(stop + FLT_EPSILON));
-        if (include_zero) {
-            bool contains_zero = false;
-            if (std::find(choices.begin(), choices.end(), 0) != choices.end()) {
-                contains_zero = true;
-            }
-            if (!contains_zero) {
-                choices.insert(choices.begin(), 0);
-            }
         }
-        return choices;
-    }
+};
+
+class ObjectOrientedActionSpace
+{
+    public:
+        ObjectOrientedActionSpace(Pose pose,
+                                  int samples,
+                                  double lin_min,
+                                  double lin_max,
+                                  double lin_samples,
+                                  double dur_min,
+                                  double dur_max,
+                                  double dur_samples)
+        : pose(pose),
+          samples(samples),
+          lin_min(lin_min),
+          lin_max(lin_max),
+          lin_samples(lin_samples),
+          dur_min(dur_min),
+          dur_max(dur_max),
+          dur_samples(dur_samples)
+        { generate_actions();
+        }
+
+    private:
+        Pose pose{ 0, 0, 0, 0 };
+        double samples{ 0 };
+        double lin_min{ 0 };
+        double lin_max{ 0 };
+        double lin_samples{ 0 };
+        double dur_min{ 0 };
+        double dur_max{ 0 };
+        double dur_samples{ 0 };
+        std::list<Object_Oriented_Action> actions;
+
+        void generate_actions(double h_offset=40, double v_offset=60)
+        {
+            generate_offsets(h_offset, v_offset);
+        }
+
+        void generate_offsets(double h_offset, double v_offset)
+        {
+            std::vector<double> choices;
+            if (samples == 1) {
+                choices.push_back(0);
+            } else {
+                choices = create_choices(-h_offset, h_offset, samples, true);
+            }
+            find_sides(pose.angle_z);
+
+        }
+
+        std::vector<double> find_sides(double angle)
+        {
+            // std::cout << angle;
+            std::vector<double> sides;
+            sides.push_back(angle);
+            for (int i = 0; i < 3; i++) {
+                angle -= M_PI / 2;
+                std::cout << angle;
+            }
+            return sides;
+        }
 };
 
 
 int main() {
     GenericActionSpace gen_space (10, 100, 5, 10, 100, 5, 1, 5, 5);
-    std::list<action> actions = gen_space.get_action_space();
+    std::list<Action> actions = gen_space.get_action_space();
+    for (auto a : actions) {
+        std::cout << a.lin_vel << ", " << a.ang_vel << ", " << a.duration << "\n";
+    }
+
+    Pose pose{100, 200, 10, 0.5};
+    ObjectOrientedActionSpace oos (pose, 3, 10, 100, 3, 1, 5, 3);
 }
