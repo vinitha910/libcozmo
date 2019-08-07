2b7c7ccc5ba08bac6b7250bb0f6458ea72eb0cf8
JHLee0513 <JoonHoHere@gmail.com>
2019-08-06 18:12:33 -0700

    cpplint utils.hpp

--- include/utils/utils.hpp
+++ include/utils/utils.hpp
@@ -1,11 +1,41 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2019, Vinitha Ranganeni, Brian Lee
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     1. Redistributions of source code must retain the above copyright notice
+//        this list of conditions and the following disclaimer.
+//     2. Redistributions in binary form must reproduce the above copyright
+//        notice, this list of conditions and the following disclaimer in the
+//        documentation and/or other materials provided with the distribution.
+//     3. Neither the name of the copyright holder nor the names of its
+//        contributors may be used to endorse or promote products derived from
+//        this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef COZMO_UTILS_HPP_
+#define COZMO_UTILS_HPP_
+
 #include <vector>
 
 namespace libcozmo {
 namespace utils {
+
 // https://gist.github.com/lorenzoriano/5414671
-
-// Generates linspace given two values
-
 template <typename T>
 std::vector<T> linspace(T a, T b, size_t N) {
     T h = (b - a) / static_cast<T>(N-1);
@@ -15,7 +45,9 @@ std::vector<T> linspace(T a, T b, size_t N) {
     for (x = xs.begin(), val = a; x != xs.end(); ++x, val += h)
         *x = val;
     return xs;
-};
+}
 
-} //  namespace utils
-} //  namespace libcozmo
+}  //  namespace utils
+}  //  namespace libcozmo
+
+#endif  // COZMO_UTILS_HPP_
