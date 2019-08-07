c0c11a89b666f5ceef5c3e3a1b2ebdf76fd23881
JHLee0513 <JoonHoHere@gmail.com>
2019-08-06 19:19:34 -0700

    core functions for Generic Action Space

--- include/action_space/generic_action_space.hpp
+++ include/action_space/generic_action_space.hpp
@@ -19,7 +19,7 @@ class GenericAction {
     GenericAction (
         const double& speed,
         const double& duration,
-        const double& direction) : \
+        const Eigen::Vector2d& direction) : \
         m_speed(speed),
         m_duration(duration),
         m_direction(direction) {}
@@ -32,52 +32,69 @@ class GenericAction {
 
     const double m_speed;
     const double m_duration;
-    const double m_direction;
+    const Eigen::Vector2d m_direction;
 };
 
 // Actionspace for Cozmo
 class GenericActionSpace {
   public:
+
+    // Constructor
+    
+    // \param min_speed The minimum speed of action
+    // \param max_speed The maximum speed of action
+    // \param min_duration The minimum duration of action
+    // \param max_duration The maximum duration of action
+    // \param num_vals The number of values for action linspace
+    // \param num_theta The number of theta values
     GenericActionSpace(
         const double& min_speed,
         const double& max_speed,
         const double& min_duration,
         const double& max_duration,
-        const int& num_vals) : \
-        m_min_speed(min_speed),
-        m_max_speed(max_speed),
-        m_min_duration(min_duration),
-        m_max_duration(max_duration),
-        m_num_vals(num_vals) {
-            std::vector<double> speeds = utils::linspace(m_min_speed, m_max_speed, static_cast<size_t>(m_num_vals));
-            std::vector<double> durations = utils::linspace(m_min_duration, m_max_duration, m_num_vals);
-            std::vector<Eigen::Vector2d> directions;
-            for i in range(int i = 0; i < m_num_vals; i++) {
-              double angle = i * M_PI / static_cast<double>(m_num_vals);
-              directions.push_back(Eigen::Vector2d(acos(angle), asin(angle)));
+        const int& num_vals,
+        const int& num_theta) : \
+        m_num_vals(num_vals),
+        m_num_theta(num_theta) {
+            m_speeds = utils::linspace(min_speed, max_speed, static_cast<size_t>(m_num_vals));
+            m_durations = utils::linspace(min_duration, max_duration, m_num_vals);
+            for (int i = 0; i < m_num_theta; i++) {
+              double angle = i * M_PI / static_cast<double>(m_num_theta);
+              m_directions.push_back(Eigen::Vector2d(acos(angle), asin(angle)));
             }
             
-            // for (int j = 0; j < m_num_vals; j++) {
-            //     for (int k = 0; k < m_num_vals; k++) {
-            //         m_actions.push_back(GenericAction())
-            //     }
-            // }
-        
+            for (int j = 0; j < m_num_vals; j++) {
+                for (int k = 0; k < m_num_vals; k++) {
+                  for (int l = 0; l < m_num_theta; l++) {
+                    m_actions.push_back(GenericAction(
+                      m_speeds[j], 
+                      m_durations[k], 
+                      m_directions[l]));
+                  } 
+                }
+            }
         }
     ~GenericActionSpace() {}
 
-    int get_action();
+    // Assigns pointer to action given action ID
 
+    // \param action_id The action ID
+    // \param action Pointer for chosen action
+    void get_action(int& action_id, GenericAction* action);
 
+    // Executes action given action ID
+
+    // \param action_id The action ID
+    void execute_action(int& action_id) const;
 
   private:
-    const double m_min_speed;
-    const double m_max_speed;
-    const double m_min_duration;
-    const double m_max_duration;
+    std::vector<double> m_speeds;
+    std::vector<double> m_durations;
     // number of "bins" to assign linspace out of 
     const int m_num_vals;
-    std::vector<Action> m_actions;
+    const int m_num_theta;
+    std::vector<GenericAction> m_actions;
+    std::vector<Eigen::Vector2d> m_directions;
 
 };
 
--- src/action_space/generic_action_space.cpp
+++ src/action_space/generic_action_space.cpp
@@ -3,8 +3,16 @@
 namespace libcozmo {
 namespace actionspace {
 
-int GenericAction::get_action() {
-    return 0;
+double GenericAction::action_similarity(GenericAction& other_action) const {
+    return sqrt(
+        pow((m_speed - other_action.m_speed), 2) + 
+        pow((m_duration - other_action.m_duration), 2) + 
+        pow((m_direction.x() - other_action.m_direction.x()), 2) +
+        pow((m_direction.y() - other_action.m_direction.y()), 2));
+}
+
+void GenericActionSpace::get_action(int& action_id, GenericAction* action) {
+    action = &m_actions[action_id];
 }
 
 }
