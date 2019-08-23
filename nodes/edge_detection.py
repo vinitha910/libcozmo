8a02f0e258d795c65378a088e4b51717bd13b3a6
Eric Pan <ericpan0207@gmail.com>
2019-08-23 15:13:11 -0700

    working edge detection using 4 edges

--- nodes/edge_detection.py
+++ nodes/edge_detection.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python3
 import cozmo
-from cozmo.util import degrees, distance_mm, speed_mmps
+from cozmo.util import degrees
 import matplotlib.pyplot as plt
 
+# Finds the corner coordinates of a 4 sided surface
+# Assumes Cozmo starts perpendicular to an edge
 def cozmo_program(robot: cozmo.robot.Robot):
-    # robot.is_cliff_detected:
     x = []
     y = []
-    counter = 0
-    done = False
+    corners = []
     robot.set_lift_height(1).wait_for_completed()
-    while not done:
-        robot.drive_wheels(25, 25, duration=1)
-        if robot.is_cliff_detected:
-            x.append(robot.pose.position.x)
-            y.append(robot.pose.position.y)
-            robot.drive_wheels(-100, -100, duration=1.5)
-            robot.turn_in_place(degrees(90)).wait_for_completed()
-            counter += 1
-        if counter == 10:
-            done = True
-         
-    plt.figure()
+
+    for angle in [180, 90, 180, 180]:
+        while not robot.is_cliff_detected:
+            robot.drive_wheels(100, 100, duration=0.25)
+        x.append(robot.pose.position.x)
+        y.append(robot.pose.position.y)
+        robot.drive_wheels(-100, -100, duration=1.5)
+        robot.turn_in_place(degrees(angle)).wait_for_completed()
+
+    corners_x = [x[1], x[0], x[1], x[0]]
+    corners_y = [y[2], y[2], y[3], y[3]]
+    corners.append((x[1], y[2]))
+    corners.append((x[0], y[2]))
+    corners.append((x[1], y[3]))
+    corners.append((x[0], y[3]))
+
+    plt.figure(0)
     plt.scatter(x, y)
+    plt.figure(1)
+    plt.scatter(corners_x, corners_y)
     plt.show()
+
 cozmo.run_program(cozmo_program)
