a23c5804b9b1cf9b79c7d7ab20f62de855fe24b2
Eric Pan <ericpan0207@gmail.com>
2019-08-22 18:33:09 -0700

    working -ish edge detection

--- nodes/edge_detection.py
+++ nodes/edge_detection.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
 import cozmo
 from cozmo.util import degrees, distance_mm, speed_mmps
+import matplotlib.pyplot as plt
 
 def cozmo_program(robot: cozmo.robot.Robot):
     # robot.is_cliff_detected:
+    x = []
+    y = []
+    counter = 0
     done = False
+    robot.set_lift_height(1).wait_for_completed()
     while not done:
-        drive = robot.drive_wheel_motors(50, 50)
+        robot.drive_wheels(25, 25, duration=1)
         if robot.is_cliff_detected:
-            robot.stop_all_motors()
+            x.append(robot.pose.position.x)
+            y.append(robot.pose.position.y)
+            robot.drive_wheels(-100, -100, duration=1.5)
+            robot.turn_in_place(degrees(90)).wait_for_completed()
+            counter += 1
+        if counter == 10:
             done = True
-    #if robot.is_cliff_detected:
-    #    robot.stop(drive)
-    
-    robot.world.request_nav_memory_map(0.1)
-    memory_map = robot.world.wait_for(cozmo.nav_memory_map.EvtNewNavMemoryMap)
-    print(memory_map.nav_memory_map)
+         
+    plt.figure()
+    plt.scatter(x, y)
+    plt.show()
 cozmo.run_program(cozmo_program)
