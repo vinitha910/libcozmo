e878cbb0b06cd1e892294225a3c94113f9bd674b
Eric Pan <ericpan0207@gmail.com>
2019-08-23 15:51:56 -0700

    Better plot

--- nodes/edge_detection.py
+++ nodes/edge_detection.py
@@ -21,15 +21,21 @@ def cozmo_program(robot: cozmo.robot.Robot):
 
     corners_x = [x[1], x[0], x[1], x[0]]
     corners_y = [y[2], y[2], y[3], y[3]]
-    corners.append((x[1], y[2]))
-    corners.append((x[0], y[2]))
-    corners.append((x[1], y[3]))
-    corners.append((x[0], y[3]))
 
     plt.figure(0)
     plt.scatter(x, y)
-    plt.figure(1)
+    plt.plot(x[0:2], y[0:2], 'r')
+    plt.plot(x[2:4], y[2:4], 'r')
     plt.scatter(corners_x, corners_y)
+    
+    a = [corners_x[i] for i in range(len(x)) if i % 2 == 0]
+    b = [corners_y[i] for i in range(len(y)) if i % 2 == 0]
+    c = [corners_x[i] for i in range(len(x)) if i % 2 != 0]
+    d = [corners_y[i] for i in range(len(y)) if i % 2 != 0]
+    plt.plot(corners_x[0:2], corners_y[0:2], 'b')
+    plt.plot(corners_x[2:4], corners_y[2:4], 'b')
+    plt.plot(a, b, 'b')
+    plt.plot(c, d, 'b')
     plt.show()
 
 cozmo.run_program(cozmo_program)
