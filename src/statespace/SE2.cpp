91ade7c67b5bb6dce23cc3b65481c08be3ccb847
JHLee0513 <JoonHoHere@gmail.com>
2019-09-06 17:12:17 -0700

    minor style edit

--- src/statespace/SE2.cpp
+++ src/statespace/SE2.cpp
@@ -142,9 +142,7 @@ void SE2::copy_state(
     *destination = State(source.x, source.y, source.theta);
 }
 
-double SE2::get_resolution() const {
-    return m_resolution;
-}
+double SE2::get_resolution() const { return m_resolution; }
 
 StateSpace::State* SE2::create_state() {
     m_state_map.push_back(new State());
