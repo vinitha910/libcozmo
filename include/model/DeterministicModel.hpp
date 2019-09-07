cf5daae058bdb2f8a66bcc9664c3f0fee7a008da
JHLee0513 <JoonHoHere@gmail.com>
2019-09-06 18:33:26 -0700

    abstract and generic model complete with tests

---
+++ include/model/DeterministicModel.hpp
@@ -0,0 +1,115 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2019,  Brian Lee, Vinitha Ranganeni
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     1. Redistributions of source code must retain the above copyright notice
+//        this list of conditions and the following disclaimer.
+//     2. Redistributions in binary form must reproduce the above copyright
+//        notice, this list of conditions and the following disclaimer in the
+//        documentation and/or other materials provided with the distribution.
+//     3. Neither the name of the copyright holder nor the names of its
+//        contributors may be used to endorse or promote products derived from
+//        this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef LIBCOZMO_MODEL_GENERICMODEL_HPP
+#define LIBCOZMO_MODEL_GENERICMODEL_HPP
+
+#include <Eigen/Dense>
+#include "model/Model.hpp"
+#include "actionspace/generic_action_space.hpp"
+
+namespace AS = libcozmo::actionspace;
+
+namespace libcozmo {
+namespace model {
+
+/// Generic model to predict Cozmo's movement
+/// Given action, delta x, y, theta is calculated
+class DeterministicModel : public virtual Model {
+ public:
+    class ModelType : public Model::ModelType {
+     public:
+     	/// Constructs weight and bias term with given parameters
+     	ModelType(const double& resolution) : \
+            m_resolution(resolution) {}
+
+     	~ModelType() = default;
+
+        double get_resolution() { return m_resolution; }
+
+     private:
+        double m_resolution;
+    };
+	
+	class ModelInput : public Model::ModelInput {
+	 public:
+	 	/// Constructs input with given parameters
+	 	ModelInput(const AS::GenericActionSpace::Action& action) : \
+	 		m_action(action) {}
+
+	 	~ModelInput() = default;
+
+	 	double get_speed() { return m_action.m_speed; }
+        double get_duration() { return m_action.m_duration; }
+        double get_heading() { return m_action.m_heading; }
+	 private:
+	 	const AS::GenericActionSpace::Action m_action;
+    };
+	
+	class ModelOutput : public Model::ModelOutput {
+	 public:
+	 	/// Constructs output with given parameters
+	 	ModelOutput(const double& x, const double& y, const double& theta) : \
+	 		m_x(x), m_y(y), m_theta(theta) {}
+
+	 	~ModelOutput() = default;
+	 	double getX() { return m_x; }
+        double getY() { return m_y; }
+        double getTheta() { return m_theta; }
+
+	 private:
+	 	double m_x;
+	 	double m_y;
+        double m_theta;
+    };
+
+    /// Identitiy constructor, model not taken as argument
+    DeterministicModel(): \
+    	m_regressor(nullptr) {}
+    ~DeterministicModel() {}
+
+    /// Loads a Model
+    ///
+    /// \param model The regression model
+    /// \return true if succesfully loaded; false otherwise
+    bool load_model(Model::ModelType* model) override;
+
+    /// Predict delta state given action
+    ///
+    /// \param input The model input
+    /// \return delta state (distance, dtheta)
+    Model::ModelOutput* get_prediction(const Model::ModelInput& input) override;
+
+ private:
+	Model::ModelType* m_regressor;
+};
+}  // namespace model
+}  // namespace libcozmo
+
+#endif  // LIBCOZMO_MODEL_GENERICMODEL_HPP
---
+++ tests/model/test_DeterministicModel.cpp
@@ -0,0 +1,106 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2019,  Brian Lee, Vinitha Ranganeni
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     1. Redistributions of source code must retain the above copyright notice
+//        this list of conditions and the following disclaimer.
+//     2. Redistributions in binary form must reproduce the above copyright
+//        notice, this list of conditions and the following disclaimer in the
+//        documentation and/or other materials provided with the distribution.
+//     3. Neither the name of the copyright holder nor the names of its
+//        contributors may be used to endorse or promote products derived from
+//        this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+////////////////////////////////////////////////////////////////////////////////
+
+#include <gtest/gtest.h>
+#include "model/DeterministicModel.hpp"
+#include <iostream>
+
+namespace AS = libcozmo::actionspace;
+
+namespace libcozmo {
+namespace model {
+namespace test {
+
+class DeterministicModelTest: public ::testing::Test {
+ public:
+    DeterministicModelTest() :\
+    	m_type(0.1),
+    	m_model() {}
+
+    void SetUp() {}
+
+    void TearDown() {}
+
+    ~DeterministicModelTest() {}
+
+    DeterministicModel m_model;
+    DeterministicModel::ModelType m_type;
+};
+
+/// Check that model without anything loaded simply handles nullptr,
+/// and that Lin Reg weights & bias are updated correctly
+TEST_F(DeterministicModelTest, LoadModelTest) {
+	DeterministicModel::ModelInput input = DeterministicModel::ModelInput(
+		AS::GenericActionSpace::Action(0, 0, 0));
+	EXPECT_TRUE(m_model.get_prediction(input) == nullptr);
+	EXPECT_TRUE(m_model.load_model(&m_type));
+}
+
+/// Check that model outputs correct delta values for each action
+TEST_F(DeterministicModelTest, GetPredictionTest) {
+	m_model.load_model(&m_type);
+	DeterministicModel::ModelInput input1 = DeterministicModel::ModelInput(
+		AS::GenericActionSpace::Action(0, 0, 0));
+	DeterministicModel::ModelInput input2 = DeterministicModel::ModelInput(
+		AS::GenericActionSpace::Action(50, 1, M_PI / 2.0));
+	DeterministicModel::ModelInput input3 = DeterministicModel::ModelInput(
+		AS::GenericActionSpace::Action(-50, 1, M_PI));
+
+	DeterministicModel::ModelOutput output1 = 
+		*static_cast<DeterministicModel::ModelOutput*>(
+			m_model.get_prediction(input1));
+	DeterministicModel::ModelOutput output2 = 
+		*static_cast<DeterministicModel::ModelOutput*>(
+			m_model.get_prediction(input2));
+	DeterministicModel::ModelOutput output3 = 
+		*static_cast<DeterministicModel::ModelOutput*>(
+			m_model.get_prediction(input3));
+
+	Eigen::Vector3d result1(output1.getX(), output1.getY(), output1.getTheta());
+
+	EXPECT_NEAR(output1.getX(), 0, 0.001);
+	EXPECT_NEAR(output1.getY(), 0, 0.001);
+	EXPECT_NEAR(output1.getTheta(), 0, 0.001);
+	EXPECT_NEAR(output2.getX(), 0, 0.001);
+	EXPECT_NEAR(output2.getY(), 5, 0.001);
+	EXPECT_NEAR(output2.getTheta(), M_PI / 2.0, 0.001);
+	EXPECT_NEAR(output3.getX(), 5, 0.001);
+	EXPECT_NEAR(output3.getY(), 0, 0.001);
+	EXPECT_NEAR(output3.getTheta(), M_PI, 0.001);
+
+}
+
+}  // namespace test
+}  // namespace model
+}  // namespace libcozmo
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
