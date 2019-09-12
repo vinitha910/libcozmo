29578246d58216e23adbc04d28b41e73cf29a7bd
JHLee0513 <JoonHoHere@gmail.com>
2019-09-12 15:19:06 -0700

    fixed testing

--- tests/distance/test_distance.cpp
+++ tests/distance/test_distance.cpp
@@ -37,64 +37,38 @@ namespace libcozmo {
 namespace distance {
 namespace test {
 
-class DistanceTest: public ::testing::Test {
- public:
-    DistanceTest() : \
-        m_statespace(statespace::SE2(0.1, 8)),
-        m_se2(distance::SE2(std::make_shared<statespace::SE2>(0.1, 8))),
-        m_trans(distance::Translation(
-            std::make_shared<statespace::SE2>(0.1, 8))),
-        m_orient(distance::Orientation(
-            std::make_shared<statespace::SE2>(0.1, 8))) {}
-
-    void SetUp() {
-        m_id_1 =
-            m_statespace.get_or_create_state(statespace::SE2::State(0, 0, 1));
-        m_id_2 =
-            m_statespace.get_or_create_state(statespace::SE2::State(1, 3, 3));
-    }
-
-    ~DistanceTest() {}
-
-    distance::SE2 m_se2;
-    distance::Translation m_trans;
-    distance::Orientation m_orient;
-    statespace::SE2 m_statespace;
-    int m_id_1;
-    int m_id_2;
-};
-
-TEST_F(DistanceTest, TestSE2) {
+TEST(DistanceTest, TestSE2) {
     // Cheking SE2 distance calculated correctly
-    const auto s1 = m_statespace.get_state(m_id_1);
-    const auto s2 = m_statespace.get_state(m_id_2);
-    EXPECT_FALSE(s1 == nullptr);
-    EXPECT_FALSE(s2 == nullptr);
+    const auto statespace = std::make_shared<statespace::SE2>(0.1, 8);
+    distance::SE2 se2 = distance::SE2(statespace);
     EXPECT_NEAR(
         sqrt(pow(0.1, 2) + pow(0.3, 2) + pow(M_PI / 2, 2)),
-        m_se2.get_distance(
-            * m_statespace.get_state(m_id_1),
-            * m_statespace.get_state(m_id_2)),
+        se2.get_distance(
+            statespace::SE2::State(0, 0, 1), statespace::SE2::State(1, 3, 3)),
         0.01);
 }
 
-TEST_F(DistanceTest, TestTranslation) {
+TEST(DistanceTest, TestTranslation) {
     // Cheking translation calculated correctly
+    const auto statespace = std::make_shared<statespace::SE2>(0.1, 8);
+    distance::Translation trans = distance::Translation(statespace);
     EXPECT_NEAR(
         sqrt(pow(0.1, 2) + pow(0.3, 2)),
-        m_trans.get_distance(
-            * m_statespace.get_state(m_id_1),
-            * m_statespace.get_state(m_id_2)),
+        trans.get_distance(
+            statespace::SE2::State(0, 0, 1),
+            statespace::SE2::State(1, 3, 3)),
         0.01);
 }
 
-TEST_F(DistanceTest, TestOrientation) {
+TEST(DistanceTest, TestOrientation) {
     // Cheking orientation calculated correctly
+    const auto statespace = std::make_shared<statespace::SE2>(0.1, 8);
+    distance::Orientation orient = distance::Orientation(statespace);
     EXPECT_NEAR(
         M_PI / 2,
-        m_orient.get_distance(
-            * m_statespace.get_state(m_id_1),
-            * m_statespace.get_state(m_id_2)),
+        orient.get_distance(
+            statespace::SE2::State(0, 0, 1),
+            statespace::SE2::State(1, 3, 3)),
         0.01);
 }
 
