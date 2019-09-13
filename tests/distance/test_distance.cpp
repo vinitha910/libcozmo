33207eaa450e701e9aa3d9d4ef6db7048dd24391
JHLee0513 <JoonHoHere@gmail.com>
2019-09-13 14:09:36 -0700

    fixed merge for distance test

--- tests/distance/test_distance.cpp
+++ tests/distance/test_distance.cpp
@@ -1,9 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
-<<<<<<< HEAD
 // Copyright (c) 2019, Vinitha Ranganeni, Brian Lee
-=======
 // Copyright (c) 2019, Brian Lee, Vinitha Ranganeni
->>>>>>> a7fffa012325937d70466fdf4b5b0938ff1a8d82
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -41,60 +38,6 @@ namespace libcozmo {
 namespace distance {
 namespace test {
 
-<<<<<<< HEAD
-class DistanceTest: public ::testing::Test {
- public:
-    DistanceTest() : \
-        m_statespace(statespace::SE2(0.1, 8)),
-        m_se2(distance::SE2(&m_statespace)),
-        m_trans(distance::Translation(&m_statespace)),
-        m_orient(distance::Orientation(&m_statespace)) {}
-
-    void SetUp() {
-        m_id_1 =
-            m_statespace.get_or_create_state(statespace::SE2::State(0, 0, 0));
-        m_id_2 =
-            m_statespace.get_or_create_state(statespace::SE2::State(1, 3, 1));
-    }
-
-    ~DistanceTest() {}
-
-    statespace::SE2 m_statespace;
-    distance::SE2 m_se2;
-    distance::Translation m_trans;
-    distance::Orientation m_orient;
-    int m_id_1;
-    int m_id_2;
-};
-
-TEST_F(DistanceTest, TestSE2) {
-    // Cheking SE2 distance calculated correctly
-    EXPECT_NEAR(
-        sqrt(pow(0.1,2) + pow(0.3,2) + pow(M_PI/4, 2)), 
-        m_se2.get_distance(
-            *m_statespace.get_state(m_id_1),
-            *m_statespace.get_state(m_id_2)),
-        0.01);
-}
-
-TEST_F(DistanceTest, TestTranslation) {
-    // Cheking translation calculated correctly
-    EXPECT_NEAR(
-        sqrt(pow(0.1,2) + pow(0.3,2)), 
-        m_trans.get_distance(
-            *m_statespace.get_state(m_id_1),
-            *m_statespace.get_state(m_id_2)),
-        0.01);
-}
-
-TEST_F(DistanceTest, TestOrientation) {
-    // Cheking orientation calculated correctly
-    EXPECT_NEAR(
-        M_PI/4, 
-        m_orient.get_distance(
-            *m_statespace.get_state(m_id_1),
-            *m_statespace.get_state(m_id_2)),
-=======
 TEST(DistanceTest, TestSE2) {
     // Checking SE2 distance calculated correctly
     const auto statespace = std::make_shared<statespace::SE2>(0.1, 8);
@@ -127,23 +70,14 @@ TEST(DistanceTest, TestOrientation) {
         orient.get_distance(
             statespace::SE2::State(0, 0, 1),
             statespace::SE2::State(1, 3, 3)),
->>>>>>> a7fffa012325937d70466fdf4b5b0938ff1a8d82
         0.01);
 }
 
 }  // namespace test
-<<<<<<< HEAD
 }  // namspace distance
-=======
-}  // namespace distance
->>>>>>> a7fffa012325937d70466fdf4b5b0938ff1a8d82
 }  // namespace libcozmo
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> a7fffa012325937d70466fdf4b5b0938ff1a8d82
