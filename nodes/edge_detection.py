5d84ae3609695ed973553cbefe224570593417e8
Eric Pan <ericpan0207@gmail.com>
2019-09-23 16:12:11 -0700

    Revised structure and working now with threshold

--- nodes/edge_detection.py
+++ nodes/edge_detection.py
@@ -8,126 +8,115 @@ import matplotlib.pyplot as plt
 
 from visualization_msgs.msg import Marker
 
-# Finds the corner coordinates of a 4 sided surface
-# Assumes Cozmo starts perpendicular to an edge
-# Cozmo will visit each edge once
-def perpendicular_detector(robot: cozmo.robot.Robot):
-    x_min = sys.maxsize
-    x_max = -sys.maxsize - 1
-    y_min = sys.maxsize
-    y_max = -sys.maxsize - 1
-    x = []
-    y = []
+class CornerDetector(object):
+    """
+    A corner detector class that uses cozmo's sensor to map the dimensions of a
+    rectangular surface
+    """
+    def __init__(self, cozmo, threshold):
+        """
+        Parameters
+        ----------
+        cozmo : cozmo.robot
+            the cozmo SDK robot handle
+        threshold : float
+            the distance away from an edge to be within bounds, in mm
+        """
+        self.cozmo = cozmo
+        self.x_min = sys.maxsize
+        self.x_max = -sys.maxsize - 1
+        self.y_min = sys.maxsize
+        self.y_max = -sys.maxsize - 1
+        self.threshold = threshold
 
-    corners = []
-    robot.set_lift_height(1).wait_for_completed()
+    def find_corners(self):
+        """
+        Finds the corner coordinates of a 4 sided surface with the constraint
+        that cozmo needs to start perpendicular to an edge
+        """
+        x = []
+        y = []
 
-    for angle in [180, 90, 180, 180]:
-        while not robot.is_cliff_detected:
-            robot.drive_wheels(100, 100, duration=0.25)
-        x_curr = robot.pose.position.x
-        y_curr = robot.pose.position.y
-        x.append(x_curr)
-        y.append(y_curr)
+        corners = []
+        self.cozmo.set_lift_height(1).wait_for_completed()
 
-        x_min = min(x_curr, x_min)
-        x_max = max(x_curr, x_max)
-        y_min = min(y_curr, y_min)
-        y_max = max(y_curr, y_max)
-        robot.drive_wheels(-100, -100, duration=1.5)
-        robot.turn_in_place(degrees(angle)).wait_for_completed()
+        # The order in which cozmo will turn and find edges
+        for angle in [180, 90, 180, 180]:
+            while not self.cozmo.is_cliff_detected:
+                self.cozmo.drive_wheels(100, 100, duration=0.25)
+            x_curr = self.cozmo.pose.position.x
+            y_curr = self.cozmo.pose.position.y
+            x.append(x_curr)
+            y.append(y_curr)
 
-    plt.figure(0)
-    plt.scatter(x, y)
-    plt.plot(x[0:2], y[0:2], 'r')
-    plt.plot(x[2:4], y[2:4], 'r')
+            self.x_min = min(x_curr, self.x_min)
+            self.x_max = max(x_curr, self.x_max)
+            self.y_min = min(y_curr, self.y_min)
+            self.y_max = max(y_curr, self.y_max)
+            self.cozmo.drive_wheels(-100, -100, duration=1.5)
+            self.cozmo.turn_in_place(degrees(angle)).wait_for_completed()
 
-    plt.scatter([x_min, x_min, x_max, x_max], [y_min, y_max, y_min, y_max])
-    plt.plot([x_min, x_max], [y_min, y_min], 'b')
-    plt.plot([x_min, x_max], [y_max, y_max], 'b')
-    plt.plot([x_min, x_min], [y_min, y_max], 'b')
-    plt.plot([x_max, x_max], [y_min, y_max], 'b')
+    def within_bounds(self, x, y):
+        """
+        Given x, y coordinates (in mm), returns if the coordinates are within bounds
+        """
+        return self.x_min + threshold <= x <= self.x_max - threshold and \
+               self.y_min + threshold <= y <= self.y_max - threshold
 
-    plt.savefig('edge.png')
-    plt.show()
+    def publish_plane(self, pub, x, y, z, color=(0, 0.5, 0.5)):
+        """
+        Publish the surface found based on x and y dimensions
 
-# Finds the corner coordinates of a 4 sided surface
-# Cozmo will continuously find coordinates of the edges of the surface
-# until new coordinates are no longer expanding the known dimensions
-def generic_detector(robot: cozmo.robot.Robot):
-    x_min = sys.maxsize
-    x_max = -sys.maxsize - 1
-    y_min = sys.maxsize
-    y_max = -sys.maxsize - 1
+        Parameters
+        ----------
+        pub : ros publisher
+        x : float
+            The x dimension of the surface
+        y : float
+            The y dimension of the surface
+        z : float
+            The height of the surface
+        All position and scale units are in mm
+        """
+        plane_marker = Marker()
+        plane_marker.header.frame_id = "base_link"
+        plane_marker.type = Marker.CUBE
 
-    x = []
-    y = []
+        plane_marker.pose.position.x = 0
+        plane_marker.pose.position.y = 0
+        plane_marker.pose.position.z = z / 1000
 
-    corners = []
-    robot.set_lift_height(1).wait_for_completed()
-    done = False
+        plane_marker.pose.orientation.x = 0
+        plane_marker.pose.orientation.y = 0
+        plane_marker.pose.orientation.z = 0
+        plane_marker.pose.orientation.w = 1
 
-    while not done:
-        while not robot.is_cliff_detected:
-            robot.drive_wheels(100, 100, duration=0.25)
-        x_curr = robot.pose.position.x
-        y_curr = robot.pose.position.y
-        x.append(x_curr)
-        y.append(y_curr)
-        robot.drive_wheels(-100, -100, duration=1.5)
+        plane_marker.scale.x = x / 1000.0
+        plane_marker.scale.y = y / 1000.0
+        plane_marker.scale.z = 1 / 1000.0
 
-        robot.turn_in_place(degrees(91)).wait_for_completed()
+        plane_marker.color.r = color[0]
+        plane_marker.color.g = color[1]
+        plane_marker.color.b = color[2]
+        plane_marker.color.a = 1
 
-        x_min = min(x_curr, x_min)
-        x_max = max(x_curr, x_max)
-        y_min = min(y_curr, y_min)
-        y_max = max(y_curr, y_max)
+        pub.publish(plane_marker)
 
-        if x_min != x_curr and x_max != x_curr and y_min != y_curr and y_max != y_curr:
-            done = True
+def cozmo_run(robot: cozmo.robot):
+    detector = CornerDetector(robot, 10)
+    plane_publisher = rospy.Publisher('plane_marker', Marker, queue_size=10)
+    boundary_publisher = rospy.Publisher('boundary_marker', Marker, queue_size=10)
 
-    plt.figure(0)
-    plt.scatter(x, y)
-    plt.plot(x, y, 'r')
-
-    plt.scatter([x_min, x_min, x_max, x_max], [y_min, y_max, y_min, y_max])
-    plt.plot([x_min, x_max], [y_min, y_min], 'b')
-    plt.plot([x_min, x_max], [y_max, y_max], 'b')
-    plt.plot([x_min, x_min], [y_min, y_max], 'b')
-    plt.plot([x_max, x_max], [y_min, y_max], 'b')
-    plt.savefig('generic_edge_detector')
-
-    plt.show()
-
-# Publish the surface found based on the 4 corners
-def publish_plane(pub, dimensions):
-    plane_marker = Marker()
-    plane_marker.header.frame_id = "base_link"
-    plane_marker.type = Marker.CUBE
-
-    plane_marker.pose.position.x = 0
-    plane_marker.pose.position.y = 0
-    plane_marker.pose.position.z = 0
-
-    plane_marker.pose.orientation.x = 0
-    plane_marker.pose.orientation.y = 0
-    plane_marker.pose.orientation.z = 0
-    plane_marker.pose.orientation.w = 1
-
-    plane_marker.scale.x = dimensions[0] / 1000.0
-    plane_marker.scale.y = dimensions[1] / 1000.0
-    plane_marker.scale.z = 1 / 1000.0
-
-    plane_marker.color.r = 0.0
-    plane_marker.color.g = 0.5
-    plane_marker.color.b = 0.5
-    plane_marker.color.a = 1.0
-
-    pub.publish(plane_marker)
+    detector.find_corners()
+    x = detector.x_max - detector.x_min
+    y = detector.y_max - detector.y_min
+    while not rospy.is_shutdown():
+        detector.publish_plane(plane_publisher, x, y, 0, color=(1, 0.5, 0))
+        detector.publish_plane(boundary_publisher, x - 2 * detector.threshold, y - 2 * detector.threshold, 2)
 
 if __name__ == '__main__':
     rospy.init_node("edge_detection")
     try:
-        cozmo.run_program(perpendicular_detector)
+        cozmo.run_program(cozmo_run)
     except rospy.ROSInterruptException:
         pass
